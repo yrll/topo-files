6e9afbde305bd5e393addeb917bfca7758432da3
yrll <yrll@github.com>
2022-04-19 11:40:58 +0800

    v04.19_(rmap inconsistency bug)

---
+++ out-configs/count_config_lines.py
@@ -0,0 +1,29 @@
+import os
+
+dirr = os.getcwd()
+def count_prefix_num(file):
+    assert os.path.exists(file)
+    with open(file, 'r') as cfg:
+        set_iface_flag = False
+        lines = cfg.readlines()
+        line_numm = len(lines)
+    return line_numm
+
+if os.path.isdir(dirr):
+    data = {}
+    print(os.listdir(dirr))
+    for dirs in os.listdir(dirr):
+        # for dir in dirs:
+        dir_path = dirr+'\\'+dirs+'\\configs\\'
+        if os.path.isdir(dir_path) is False:
+            continue
+        print(dirs)
+        all = 0
+        for root1, dirs1, files1 in os.walk(dir_path):
+            for file in files1:
+                all += count_prefix_num(root1+file)
+        print(all)
+
+
+
+
